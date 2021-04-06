46ccc156bfda33f7f141f64fb5b8e75be2fcda3e
Ahire Hitesh <hbahire2014@gmail.com>
2021-04-06 23:27:47 +0530

    add Student  CRUD opration simple console base project

--- SpringORM/src/main/java/com/Spring/ORM/dao/StudentDao.java
+++ SpringORM/src/main/java/com/Spring/ORM/dao/StudentDao.java
@@ -1,5 +1,7 @@
 package com.Spring.ORM.dao;
 
+import java.util.List;
+
 import javax.transaction.Transactional;
 
 import org.springframework.orm.hibernate5.HibernateTemplate;
@@ -9,15 +11,41 @@ import com.Spring.ORM.entities.Student;
 public class StudentDao {
 
 	private HibernateTemplate hibernateTemplate;
-	
-	
+
+	// insert student
 	@Transactional
 	public int insert(Student student) {
-
-		//insert data
+		// insert data
 		return (Integer) this.hibernateTemplate.save(student);
 	}
 
+	// get single student data
+	public Student getStudent(int student_id) {
+
+		return this.hibernateTemplate.get(Student.class, student_id);
+	}
+
+	// get all student data
+	public List<Student> getAllStudent() {
+
+		return this.hibernateTemplate.loadAll(Student.class);
+	}
+
+	// deleting student data
+	@Transactional
+	public void deleteStudent(int student_id) {
+
+		this.hibernateTemplate.delete(this.getStudent(student_id));
+	}
+	
+	//update student
+	@Transactional
+	public void updateStudent(Student student){
+		
+		this.hibernateTemplate.update(student);
+	}
+
+	// getter setter
 	public HibernateTemplate getHibernateTemplate() {
 		return hibernateTemplate;
 	}
@@ -25,5 +53,5 @@ public class StudentDao {
 	public void setHibernateTemplate(HibernateTemplate hibernateTemplate) {
 		this.hibernateTemplate = hibernateTemplate;
 	}
-	
+
 }
