1219b2764b65fad52db5a05643e89b750b63f4ad
Ahire Hitesh <hbahire2014@gmail.com>
2021-04-08 11:13:58 +0530

    add jstl

--- springMVC/src/main/java/springMVC/Controller/HomeController.java
+++ springMVC/src/main/java/springMVC/Controller/HomeController.java
@@ -1,11 +1,14 @@
 package springMVC.Controller;
 
+import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.springframework.format.datetime.joda.LocalDateTimeParser;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class HomeController {
@@ -14,12 +17,12 @@ public class HomeController {
 	public String home(Model model) {
 
 		System.out.println("home calling...");
-		
-		//add data 
+
+		// add data
 		model.addAttribute("id", 1);
 		model.addAttribute("name", "Hitesh Bhagwan Ahire");
-		
-		//list 
+
+		// list
 		List<String> friends = new ArrayList<String>();
 		friends.add("Ganesh");
 		friends.add("Nikita");
@@ -30,18 +33,39 @@ public class HomeController {
 		model.addAttribute("friends", friends);
 		return "index";
 	}
-	
+
 	@RequestMapping("/about")
-	public String about(){
-		
+	public String about() {
+
 		System.out.println("about calling...");
 		return "about";
 	}
-	
+
+	// ModelAndView
 	@RequestMapping("/help")
-	public String help(){
-		
+	public ModelAndView help() {
+
 		System.out.println("help calling...");
-		return "help";
+		// creating model and view objecct
+		ModelAndView modelAndView = new ModelAndView();
+		// setting the data
+		modelAndView.addObject("id", 11);
+		modelAndView.addObject("name", "Ganesh Nikam");
+
+		modelAndView.addObject("time", LocalDateTime.now());
+		
+		//marks
+		List<Integer> list = new ArrayList<Integer>();
+		list.add(11);
+		list.add(12);
+		list.add(14);
+		list.add(15);
+		list.add(16);
+		modelAndView.addObject("marks", list);
+		
+		// set page to going to
+		modelAndView.setViewName("help");
+
+		return modelAndView;
 	}
 }
--- springMVC/src/main/webapp/WEB-INF/views/help.jsp
+++ springMVC/src/main/webapp/WEB-INF/views/help.jsp
@@ -1,5 +1,8 @@
+<%@page import="java.time.LocalDateTime"%>
 <%@ page language="java" contentType="text/html; charset=ISO-8859-1"
-    pageEncoding="ISO-8859-1"%>
+	pageEncoding="ISO-8859-1"%>
+<%@page isELIgnored="false"%>
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core"%>
 <!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
 <html>
 <head>
@@ -7,7 +10,32 @@
 <title>Help Page</title>
 </head>
 <body>
+	<%
+		/* int id = (Integer) request.getAttribute("id");
+		String name = (String) request.getAttribute("name");
+		LocalDateTime time = (LocalDateTime) request.getAttribute("time"); */
+	%>
 
-<h1> Hellow this is help page</h1>
+	<h1>
+		Id : ${id }
+
+
+		<%-- <%=id%> --%>
+	</h1>
+	<h1>
+		Name : ${name }
+		<%-- <%=name%> --%>
+	</h1>
+	<h1>
+		${time}
+		<%-- <%=time%> --%>
+	</h1>
+	<hr>
+	<c:forEach var="item" items="${marks }">
+		<%-- <h1>${item }</h1> --%>
+		<h1>
+			<c:out value="${item }"></c:out>
+		</h1>
+	</c:forEach>
 </body>
 </html>
